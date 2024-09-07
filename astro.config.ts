cf8f662756b12c96f959a33f51e53da8c8ba6d2c
okada genya <g.okada.wilf+github@gmail.com>
2024-09-07 22:32:53 +0900

    init

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "astro/config";
 import deno from "@deno/astro-adapter";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: deno(),
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
